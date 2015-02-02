31ac88df1172a3b1b6c1fc07f4e24b7e267d0855
YigWoo <wooyichao@gmail.com>
2015-02-02 21:51:00 +0800

    discard server

--- src/main/java/com/yichao/woo/netty/io/example/discard/DiscardServerHandler.java
+++ src/main/java/com/yichao/woo/netty/io/example/discard/DiscardServerHandler.java
@@ -1,17 +1,12 @@
 package com.yichao.woo.netty.io.example.discard;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-/**
- * Created by Yichao-Woo on 2015-01-31.
- */
 public class DiscardServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ((ByteBuf) msg).release();
     }
 
     @Override
